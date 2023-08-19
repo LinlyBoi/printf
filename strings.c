6a7403a7c41f70c7ca10a93cb2fd735376a5211e
Supermjork <markehabaziz@gmail.com>
2023-08-19 22:01:55 +0300

    Documented '_strlen'

--- strings.c
+++ strings.c
@@ -1,7 +1,11 @@
 #include "main.h"
 
 /**
- * _strlen - nuts
+ * _strlen - Takes string and return its length
+ *
+ * @str: Address to the head of the string (Array of Characters) (Lost btw)
+ *
+ * Returns: Length of String
  */
 int _strlen(char *str)
 {
