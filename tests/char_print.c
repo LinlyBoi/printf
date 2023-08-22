a723d77399dc4c6ba1741174c45fe9c4dab8b9b2
LinlyBoi <libkyy@e.email>
2023-08-22 14:40:37 +0300

    adjusted tests

--- tests/char_print.c
+++ tests/char_print.c
@@ -10,7 +10,6 @@ int main(void)
 {
 	char c = 97;
 
-	_printf("%c%c\n", c, c);
-	_printf("cat: %c\n", c);
+	_printf("cat: %css", c);
 	return (0);
 }
---
+++ tests/puts.c
@@ -0,0 +1,12 @@
+#include "../main.h"
+/**
+ * main - puts works?
+ */
+int main(void)
+{
+	char *str;
+
+	str = "hello\n";
+	_puts(str);
+	return (0);
+}
