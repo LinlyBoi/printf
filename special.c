fad0b9fde364bf1c1e7f8dc38457e07debae1296
Supermjork <markehabaziz@gmail.com>
2023-08-22 23:33:30 +0300

    FMAMER MOJROKKK

--- special.c
+++ special.c
@@ -10,8 +10,8 @@ int printf_37(void)
 }
 
 /**
- * printf_rot13 - printf str to ROT13 place into buffer
- * @args: type struct va_arg where is allocated printf arguments
+ * printf_rot13 -  str to ROT13 place into buffer
+ * @args:  va_arg where is allocated printf arguments
  * Return: counter
  */
 int printf_rot13(va_list args)
